4387e5f4499869d1c8c5eaace1f323f668509bef
Clara Grotehans <68223176+ClaraGrthns@users.noreply.github.com>
2021-07-29 11:51:26 +0200

    allowed adding new keys to cfgNode --> updated default yaml

--- configs/config.py
+++ configs/config.py
@@ -1,45 +1,15 @@
-import argparse
 from yacs.config import CfgNode as CN
-
+DEFAULT_YAML_PATH = "configs/experiments/default_config.yaml"
 # Configuration variables
 cfg = CN()
-
-cfg.LOGGING = CN()
-cfg.LOGGING.LOGDIR = './logging'
-cfg.LOGGING.LOG_STEPS = 200
-
-cfg.DATASETS = CN()
-cfg.DATASETS.ThreeDPW = '../3DPW'
-
-cfg.STORE_SEQUENCES = True
-cfg.STORE_IMAGES = True
-
-cfg.LOAD_FROM_ZARR = CN()
-cfg.LOAD_FROM_ZARR.TRN = None
-cfg.LOAD_FROM_ZARR.VAL = None
-
-cfg.TRAIN = CN()
-cfg.TRAIN.BATCH_SIZE_TRN = 16
-cfg.TRAIN.BATCH_SIZE_VAL = 16
-cfg.TRAIN.NUM_EPOCHS = 5
-cfg.TRAIN.LEARNING_RATE = 1.0e-4
-cfg.TRAIN.NUM_REQUIRED_KPS = 8
-
-cfg.MODEL = CN()
-cfg.MODEL.DIM_Z = 128
-cfg.MODEL.ENCODER = 'resnet50'
-
-cfg.LOSS = CN()
-cfg.LOSS.smpl = 0
-cfg.LOSS.verts = 1.
-cfg.LOSS.kp_2d = 0
-cfg.LOSS.kp_3d = 0
+cfg.set_new_allowed(is_new_allowed=True)
 
 
 def get_cfg_defaults():
     """Get a yacs CfgNode object with default values for my_project."""
     # Return a clone so that the defaults will not be altered
     # This is for the "local variable" use pattern
+    cfg.merge_from_file(DEFAULT_YAML_PATH)
     return cfg.clone()
 
 
@@ -47,4 +17,5 @@ def update_cfg(cfg_filepath):
     cfg = get_cfg_defaults()
     cfg.merge_from_file(cfg_filepath)
     return cfg.clone()
+
 
