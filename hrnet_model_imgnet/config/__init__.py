f46e8e08bb3b43ebb2fcddb8cc6cb6e05afbfde6
ClaraGrthns <clara.grotehans@hotmail.de>
2024-11-09 23:15:55 +0100

    more refactoring: renaming and updating read me

--- hrnet_model_imgnet/config/__init__.py
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# ------------------------------------------------------------------------------
-
-from .default import cfg as config
-from .default import update_hrnet_cfg
